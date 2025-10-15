534efa96e1ec53b40b5737547a5162b1c09da0e9
kwonhee1 <lkh8033@gmail.com>
2025-10-16 00:07:40 +0900

    feat CustomDelimeter
    feat domain class : CustomDelimeter
    feat constant class : Delimeter Constant

---
+++ src/main/java/calculator/constant/DelimiterConstant.java
@@ -0,0 +1,5 @@
+package calculator.constant;
+
+public class DelimiterConstant {
+    public static final String CUSTOM_DELIMITER_REGEX = "^//.+\n$";
+}
---
+++ src/main/java/calculator/domain/CustomDelimiter.java
@@ -0,0 +1,36 @@
+package calculator.domain;
+
+import calculator.constant.DelimiterConstant;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomDelimiter {
+
+    private static final Pattern pattern = Pattern.compile(DelimiterConstant.CUSTOM_DELIMITER_REGEX);
+
+    private final String customDelimiter;
+
+    private CustomDelimiter(String customDelimiter) {
+        validateIsNotEmpty(customDelimiter);
+        this.customDelimiter = customDelimiter;
+    }
+
+    public static Optional<CustomDelimiter> extractCustomDelimiter(String inputStr) {
+        String customDelimiter;
+
+        Matcher matcher = pattern.matcher(inputStr);
+        if (!matcher.find())
+            return Optional.empty();
+
+        customDelimiter = matcher.group();
+
+        return Optional.of(new CustomDelimiter(customDelimiter));
+    }
+
+    private void validateIsNotEmpty(String customDelimiter) {
+        if(customDelimiter == null || customDelimiter.isEmpty())
+            throw new IllegalArgumentException("custom regex는 비어있을 수 없습니다");
+    }
+}
